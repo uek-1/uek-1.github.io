887e5716da7b4ea36e84886f1c13d79c552664f7
Vivek <vickey2183@gmail.com>
2023-07-22 22:48:26 +0530

    rebuild

--- build/_app/immutable/chunks/paths.886e5892.js
+++
@@ -1,1 +0,0 @@
-var s;const e=((s=globalThis.__sveltekit_ht99o0)==null?void 0:s.base)??"/uek-1.github.io";var t;const a=((t=globalThis.__sveltekit_ht99o0)==null?void 0:t.assets)??e;export{a,e as b};
