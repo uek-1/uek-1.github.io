3f4c247c7de82feb7b874d97a6cf6cc9a9b5f6ee
Vivek <vickey2183@gmail.com>
2023-07-24 16:58:02 +0530

    changed css

---
+++ _app/immutable/chunks/paths.351e3b67.js
@@ -0,0 +1,1 @@
+var s;const a=((s=globalThis.__sveltekit_9akrct)==null?void 0:s.base)??"/uek-1.github.io";var t;const e=((t=globalThis.__sveltekit_9akrct)==null?void 0:t.assets)??a;export{e as a,a as b};
