71461dea2bce2131cefbff12eaa6cb7cb1f14ded
Vivek <vickey2183@gmail.com>
2023-07-25 18:30:57 +0530

    rebuild

---
+++ _app/immutable/nodes/2.735ef19c.js
@@ -0,0 +1,14 @@
+import{s as f,n as s}from"../chunks/scheduler.63274e7e.js";import{S as h,i as b,g as p,s as v,h as m,x as d,c as y,k as I,a as l,f as r}from"../chunks/index.20be6d9d.js";import{b as g}from"../chunks/paths.e7a8ea8b.js";function _(u){let e,o="Vivek Kethineni",n,i,c=`<article id="name"><center><figure><figcaption class="title svelte-15z22of">Vivek Kethineni</figcaption> <img src="${g}/group_picture.jpg" alt="group picture" class="main svelte-15z22of"/> <figcaption class="sub svelte-15z22of">Moving out of the freshman dorms (2023)</figcaption></figure></center></article> <article id="about"><h2>About</h2> <body>Hi, I&#39;m a sophmore at the University of Minnesota - Twin Cities, pursuing
+      a bachelors degree in Computer Science. In my spare time, I like reading,
+      listening to music, video games, and bouldering.</body></article> <article id="interests"><h2>Tech-related Interests</h2> <body><ol class="bold-items svelte-15z22of"><li class="svelte-15z22of">Articial Intelligence</li> <p>Specifically, I&#39;m interested in research related to running large
+          models on devices that are not powerful. For example, running a
+          language language model on a local server using a rasberry pi or a
+          used laptop. I think research in reducing the memory required to load
+          large models and speed up inference time will prove to be very useful
+          in making current artificial intellgience based products popular with
+          consumers.</p> <li class="svelte-15z22of">Compression</li> <p>Recently, I&#39;ve been very interested in compression algorithms. I find
+          them very interesting because of how effective they can be. I
+          implemented a few image compression methods in my project, <a href="${g}/projects/#pngpeg">pngpeg</a></p> <li class="svelte-15z22of">Functional Programming</li> <p>I&#39;ve been experimented with Haskell because I wanted to know how
+          coding in functional languages feel. It&#39;s been a very interesting
+          experience and has made me reconsider the way I think about certain
+          problems.</p></ol></body></article> <article id="defaults"><h2>Defaults</h2> <body><ul><li>Programming Language: <kbd>Rust</kbd></li> <li>IDE: <kbd>Helix + Zellij</kbd></li> <li>Operating System: <kbd>Pop!_OS</kbd></li></ul></body></article>`;return{c(){e=p("title"),e.textContent=o,n=v(),i=p("main"),i.innerHTML=c,this.h()},l(t){e=m(t,"TITLE",{"data-svelte-h":!0}),d(e)!=="svelte-1bfsctq"&&(e.textContent=o),n=y(t),i=m(t,"MAIN",{class:!0,"data-svelte-h":!0}),d(i)!=="svelte-nkgjxn"&&(i.innerHTML=c),this.h()},h(){I(i,"class","container")},m(t,a){l(t,e,a),l(t,n,a),l(t,i,a)},p:s,i:s,o:s,d(t){t&&(r(e),r(n),r(i))}}}class C extends h{constructor(e){super(),b(this,e,null,_,f,{})}}export{C as component};
